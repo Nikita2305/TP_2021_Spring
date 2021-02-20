d030785567bbc03dfc8a22786cc3209ac5a988fb
Nikita <nikitin.nn@phystech.edu>
2021-02-20 19:59:10 +0300

    Adding compress to tar

--- task1/run.sh
+++ task1/run.sh
@@ -4,18 +4,18 @@ content_dir=`realpath $1`
 extension=$2
 dest_dir=`realpath $3`
 arch_name=$4
+len=${#content_dir}
 
 mkdir -p $dest_dir
 
 for file in `find $content_dir -name \*.$extension`
 do
     absolute_file_path=`realpath $file`
-    len=${#content_dir}
     short_path=${absolute_file_path:len}
     mkdir -p $dest_dir/`dirname $short_path`
-    cp $file $dest_dir/$short_path
+    cp $absolute_file_path $dest_dir/$short_path
 done
 
-tar -cf $arch_name -P $dest_dir
+tar -czf $arch_name -P $dest_dir
 
 echo done
