089c7ad6b5e7d5c71d85037568ebf0fa79c6f4c3
Nikita <nikitin.nn@phystech.edu>
2021-02-20 19:13:37 +0300

    Absolute paths

--- task1/run.sh
+++ task1/run.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-content_dir=$1
+content_dir=`realpath $1`
 extension=$2
-dest_dir=$3
+dest_dir=`realpath $3`
 arch_name=$4
+
+mkdir -p $dest_dir
 
 for file in `find $content_dir -name \*.$extension`
 do
+    absolute_file_path=`realpath $file`
     len=${#content_dir}
-    short_path=${file:len}
+    short_path=${absolute_file_path:len}
     mkdir -p $dest_dir/`dirname $short_path`
     cp $file $dest_dir/$short_path
 done
 
-tar -cf $arch_name $dest_dir
+tar -cf $arch_name -P $dest_dir
 
 echo done
